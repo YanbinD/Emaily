266d65bb30625c489b4da775755397ec7e1280ce
Yanbin Deng <dyb0214@gmail.com>
2018-10-05 21:28:07 -0700

    fixed PORT

--- index.js
+++ index.js
@@ -5,7 +5,5 @@ app.get('/', function(req, res){
 	res.send("hello");
 }) ;
 
-const PORT = process.env.port || 3000;
-app.listen(PORT, function() {
-	console.log("server running on port 3000");
-});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT);
