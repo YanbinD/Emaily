de24195543673ee0f680381b8acbd0f526202280
Yanbin Deng <dyb0214@gmail.com>
2018-10-10 16:35:22 -0700

    added passport call back then()

--- services/passport.js
+++ services/passport.js
@@ -10,14 +10,27 @@ const mongoose = require('mongoose');
 const User = mongoose.model('users');
 
 passport.use(new GoogleStrategy({
-		clientID: keys.googleClientID,
-		clientSecret: keys.googleClientSecret,
-		callbackURL: '/auth/google/callback'
-	}, (accessToken, refershToken, profile,done) => {
-		// console.log(accessToken);
-		// console.log(refershToken);
-		// console.log(profile);
-		console.log("new user created " + profile.id)
-		new User({googleID : profile.id }).save();
-	})
+			clientID: keys.googleClientID,
+			clientSecret: keys.googleClientSecret,
+			callbackURL: '/auth/google/callback'
+		}, 
+		(accessToken, refershToken, profile,done) => {
+			User.findOne({googleID: profile.id}).then((existingUser) => {
+	            if (existingUser) {
+	                //we already have the record for the user 
+
+	                // ** use of done **
+	                done (null, existingUser);	
+	                console.log("user exist " + existingUser)
+	            } else {
+	                //create a new user 
+	                console.log("new user created " + profile.id)
+					new User({googleID : profile.id }).save()
+					.then(user => done(null, user));
+	            }
+			});  				
+	    }
+	)
 );  
+
+
