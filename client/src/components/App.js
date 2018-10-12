b46fdb63c6a5a65eeff0ddbfc65d9b71c38bb745
Yanbin Deng <dyb0214@gmail.com>
2018-10-12 13:16:50 -0700

    import BrowserRouter and Route modle in /components/App.js and create dummy objects (L68)

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,21 @@
 import React from 'react'
 
+// step2 
+// BrowserRouter is the Brain of the react router, 
+// 		 looks as the current URL and then change the set of components 
+//		 that are visible on the screen at any given time 
+// Route is the react component that is used to setup a rule between a certain route 
+//		that the user might visit indide an applicaiton and a set of component that will be 
+//		actually visible on the screen
+import {BrowserRouter} from 'react-router-dom'; 
+
+//dummy components for demo
+const Header = () => <h2> Header </h2>
+const DashBoard = () => <h2> DashBoard </h2>
+const SurveyNew = () => <h2> SurveyNew </h2>
+const Landing = () => <h2> Landing </h2>
+
+
 const App = () => {
 	return (
 		<div> 
