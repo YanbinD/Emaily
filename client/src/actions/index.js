09bd69b7fa072e5eb98ea42dd1cd597c12dec299
Yanbin Deng <dyb0214@gmail.com>
2018-10-13 02:20:08 -0700

    refactor /client/src/action/index.js the fetchUser action creater to have the async await syntax (L80)

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,11 +1,23 @@
 import axios from 'axios'
 import { FETCH_USER } from './types';
 
-export const fetchUser = () => {
-	return function (dispatch) {
-		axios.get('/api/current_user')
-		.then (res => dispatch({ type:FETCH_USER, payload: res})) //use axios to make a get request to back end 		
-	};
-};
+//==== V1 of the code , call the dispath function after the promise from the get request return 
+//fetchUser action creater 
+// export const fetchUser = () => {
+// 	return function (dispatch) {
+// 		axios.get('/api/current_user')
+// 		.then (res => dispatch({ type:FETCH_USER, payload: res})) //use axios to make a get request to back end 		
+// 	};
+// };
 
-//==== V! of the code , call the dispath function after the promise from the get request return 
+
+//==== V2.1 
+// export const fetchUser = () =>  dispatch => {
+// 		axios.get('/api/current_user')
+// 		.then (res => dispatch({ type:FETCH_USER, payload: res})) //use axios to make a get request to back end 		
+// };
+
+export const fetchUser = () => async dispatch => {
+	const res =	await axios.get('/api/current_user')
+	dispatch({ type:FETCH_USER, payload: res}); //use axios to make a get request to back end 		
+};
