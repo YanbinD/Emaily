b838a1117e220203acc728361485ec99af68c00a
Yanbin Deng <dyb0214@gmail.com>
2018-10-12 19:37:36 -0700

    using redux-thung with action creater v1 (L77)

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,5 +2,10 @@ import axios from 'axios'
 import { FECTH_USER } from './types';
 
 const fetchUser = () => {
-	axios.get('/api/current_user')//use axios to make a get request to back end 
+	return function () {
+		axios.get('/api/current_user')
+		.then (res => dispatch({ type:FECTH_USER, payload: res})) //use axios to make a get request to back end 		
+	}
 }
+
+//==== V! of the code , call the dispath function after the promise from the get request return 
