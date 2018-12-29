0645f885c81c5b96d554b94f65de09c8156bb1ac
Yanbin Deng <dyb0214@gmail.com>
2018-12-29 11:38:54 -0800

    finish Mailer.js

--- services/Mailer.js
+++ services/Mailer.js
@@ -1,15 +1,59 @@
-const sendgrid = require('sendgrid');
+const sendgrid = require("sendgrid");
 const helper = sendgrid.mail;
-const keys = require('../config/keys');
+const keys = require("../config/keys");
 
-// extends the Mail class in the sendGrid library , can be treated as a react component 
+// extends the Mail class in the sendGrid library , can be treated as a react component
 class Mailer extends helper.Mail {
+  // deconstruct the object for subject and recipients
+  constructor({ subject, recipients }, content) {
+    super();
 
-    // deconstruct the object for subject and recipients 
-    constructor ({subject, recipients}, content) {
-        
-    }
+    this.sgApi = sendgrid(keys.sendGridKey); //return the object to communicate with the send grid API 
+    this.from_email = new helper.Email("no-reply@emaily.com");
+    this.subject = subject;
+    this.body = new helper.content("text/html", content);
+    this.recipients = this.formatAddresses(recipients);
+
+    this.addContent(this.body); //addContent() defined in base class 
+    this.addClickTracking();
+    this.addRecipients();
+  }
+
+  formatAddresses(recipients) {
+    return recipients.map(({ email }) => {
+      return new helper.Email(email);
+    }); 
+  }
+
+  addClickTracking() {
+    const trackingSettings = new helper.TrackingSettings();
+    const clickTracking = new helper.ClickTracking(true, true);
+
+    trackingSettings.setClickTracking(clickTracking);
+    this.addTrackingSettings(trackingSettings);
+  }
+
+  addRecipients() {
+    const personalize = new helper.Personalization();
+
+    this.recipients.forEach(recipient => {
+      personalize.addTo(recipient);
+    });
+    this.addPersonalization(personalize);
+  }
+
+  // pack the mailer to sendGrid 
+  async send() {
+    const request = this.sgApi.emptyRequest({
+      method: 'POST',
+      path: '/v3/mail/send',
+      body: this.toJSON()
+    });
+
+    const response = await this.sgApi.API(request); 
+    return response;
+  }
+
 }
-
 
 module.exports = Mailer;
