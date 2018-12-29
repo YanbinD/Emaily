ce4b6a9f7965a547fb7fd5cf0b7d85bfe3e6114c
Yanbin Deng <dyb0214@gmail.com>
2018-12-29 01:10:10 -0800

    added surveyRoute handler

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,8 +1,19 @@
+const mongoose = require('mongoose')
 const requireLogin = require('../middlewares/requireLogin');
 const minimumCredit = require('../middlewares/minimumCredit');
 
-module.exports = app => {
-    app.app('/api/surveys', requireLogin,  (req, res) => {
+const Survey = mongoose.model('surveys'); 
 
+module.exports = app => {
+    app.app('/api/surveys', requireLogin, minimumCredit, (req, res) => {
+        const {title, subject, body, recipients} = req.body; 
+        const survey = new Survey ({
+            title,
+            subject, 
+            body, 
+            receipients: recipients.split(',').map(email => ({email : email.trim() })),
+            _user: req.user.id,
+            dateSent: Date.now()
+        });
     })
 }
