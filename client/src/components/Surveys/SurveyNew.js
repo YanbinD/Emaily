c97f02c6ae27f921c3fe891300f6354b2acc4bb8
Yanbin Deng <dyb0214@gmail.com>
2018-12-29 17:45:37 -0800

    clear form data after navigating away from survey/new & send button working

--- client/src/components/Surveys/SurveyNew.js
+++ client/src/components/Surveys/SurveyNew.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { reduxForm } from 'redux-form';
 import SurveyForm from "./SurveyForm";
 import SurveyFormReview from "./SurveyFormReview";
 
@@ -26,4 +27,6 @@ class SurveyNew extends Component {
   }
 }
 
-export default SurveyNew;
+export default reduxForm({
+    form: 'surveyForm'
+})(SurveyNew);
