9e3840ffda8ede4c204ea921d30c52074508b5b5
Yanbin Deng <dyb0214@gmail.com>
2018-12-30 11:45:35 -0800

    wired survey api to redux

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { FETCH_USER } from "./types";
+import { FETCH_USER, FETCH_SURVEYS}  from "./types";
 
 //==== V1 of the code , call the dispath function after the promise from the get request return
 //fetchUser action creater
@@ -39,3 +39,10 @@ export const submitSurvey = (values, history) => async dispatch => {
   history.push('/surveys'); //redirect after adding withRouter helper on SurveyFormReview 
   dispatch({ type: FETCH_USER, payload: res.data });
 };
+
+export const fetchSurveys = () => async dispatch => {
+  const res = await axios.get("/api/surveys");
+  dispatch({ type : FETCH_SURVEYS, payload: res.data});
+}
+
+
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,10 +1,11 @@
 import { combineReducers } from 'redux'; 
 import authReducer from './authReducer';
 import { reducer as reduxForm } from 'redux-form';
-
+import  surveysReducer  from './surveysReducer';
 
 export default combineReducers({
 	//the auth state is being manufacture by the authReducer
 	auth: authReducer, //assign the authReducer to the auth property, so our state object will have one property call auth 
-	form : reduxForm
+	form : reduxForm,
+	surveys : surveysReducer
 });
